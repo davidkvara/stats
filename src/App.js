171602b74bc5c551f233d696961a401de9cd5cd2
davidkvara <davekvaratskhelia@gmail.com>
2020-10-26 20:38:28 +0400

    first draft

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { mapCovData } from "./utils/mapData";
 import "./App.css";
+import { mapCovData, apiURL } from "./utils/mapData";
 
 function App() {
   const [items, setItems] = useState([]);
@@ -8,17 +8,14 @@ function App() {
   const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
-    fetch(
-      "https://services.arcgis.com/5T5nSi527N4F7luB/arcgis/rest/services/COVID_19_Historic_cases_by_country_pt_v7_view/FeatureServer/0/query?where=ISO_3_CODE%3D%27geo%27&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=date_epicrv%2CADM0_NAME%2CShort_Name_AR%2CShort_Name_ZH%2CShort_Name_RU%2CShort_Name_ES%2CShort_Name_FR%2CISO_3_CODE%2CNewCase%2CCaseLast7Days%2CCaseLast7DaysChange%2CCumCase%2CCumCasePop%2CNewDeath%2CDeathLast7Days%2CDeathLast7DaysChange%2CCumDeath%2CCumDeathPop%2CISO_2_CODE%2CWHO_REGION&returnGeometry=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=4326&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=true&orderByFields=date_epicrv+desc&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token="
-    )
+    fetch(apiURL)
       .then(res => res.json())
       .then(
         result => {
           setIsLoaded(true);
           // console.log(result.features.splice(0, 5));
-          setItems(mapCovData(result.features.splice(0, 5)));
+          setItems(mapCovData(result.features.splice(0, 1)));
         },
-
         error => {
           setIsLoaded(true);
           setError(error);
@@ -31,11 +28,57 @@ function App() {
     return <div>Loading...</div>;
   } else {
     // console.log(items);
+    const currentDate = new Date();
     return (
-      <div className="App">
-        <header className="App-header">
-          <pre>{JSON.stringify(items, null, 2)}</pre>
-        </header>
+      <div className="App pg-width">
+        <div className="pagehead">
+          <h1 className="h2">COVID-19-ის სტატისტიკა საქართველოში</h1>
+        </div>
+        <div className="stat-container">
+          <div className="metadata">
+            ბოლო განახლება:{" "}
+            <b>
+              {currentDate.toLocaleDateString(undefined, {
+                year: "numeric",
+                month: "long",
+                day: "numeric"
+              })}
+            </b>
+          </div>
+          <br />
+          <br />
+          {items.map(item => (
+            <div key={item.id} className="grid">
+              <article className="class list emergency ">
+                <div className="content mb-3">
+                  <div className="h1 text-bold">{item.newCases}</div>
+                  <div className="content text-bold">ახალი შემთხვევა</div>
+                </div>
+              </article>
+              <article className="class list emergency ">
+                <div className="content mb-3">
+                  <div className="h1 text-bold">{item.deathToday}</div>
+                  <div className="content text-bold">გარდაცვლილი</div>
+                </div>
+              </article>
+              <article className="class list emergency ">
+                <div className="content mb-3">
+                  <div className="h1 text-bold">{item.cases}</div>
+                  <div className="content text-bold">
+                    სულ დადასტურებული შემთხვევა
+                  </div>
+                </div>
+              </article>
+              <article className="class list emergency ">
+                <div className="content mb-3">
+                  <div className="h1 text-bold">{item.death}</div>
+                  <div className="content text-bold">სულ გარდაცვლილი</div>
+                </div>
+              </article>
+            </div>
+          ))}
+        </div>
+        <a href="https://www.who.int/countries/geo/">who.int/countries/geo/</a>
       </div>
     );
   }
--- src/utils/mapData.js
+++ src/utils/mapData.js
@@ -9,3 +9,6 @@ export const mapCovData = data => {
     deathToday: attributes.NewDeath
   }));
 };
+
+export const apiURL =
+  "https://services.arcgis.com/5T5nSi527N4F7luB/arcgis/rest/services/COVID_19_Historic_cases_by_country_pt_v7_view/FeatureServer/0/query?where=ISO_3_CODE%3D%27geo%27&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=date_epicrv%2CADM0_NAME%2CShort_Name_AR%2CShort_Name_ZH%2CShort_Name_RU%2CShort_Name_ES%2CShort_Name_FR%2CISO_3_CODE%2CNewCase%2CCaseLast7Days%2CCaseLast7DaysChange%2CCumCase%2CCumCasePop%2CNewDeath%2CDeathLast7Days%2CDeathLast7DaysChange%2CCumDeath%2CCumDeathPop%2CISO_2_CODE%2CWHO_REGION&returnGeometry=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=4326&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=true&orderByFields=date_epicrv+desc&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token=";
